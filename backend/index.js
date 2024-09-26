22da85c0a8b2154a0421746c9dc1a2fc4c7467b1
ST10033851 <ST10033851@vcconnect.edu.za>
2024-09-26 18:34:04 +0200

    Started Login And register component

--- backend/index.js
+++ backend/index.js
@@ -31,6 +31,10 @@ const options = {
 }
 
 
-https.createServer(options, app).listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+//https.createServer(options, app).listen(PORT, () => {
+ //   console.log(`Server is running on port ${PORT}`)
+//})
+
+app.listen(PORT, () => {
+    console.log(`Server us running on port ${PORT}`)
 })
