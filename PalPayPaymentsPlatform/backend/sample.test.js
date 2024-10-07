55779b52289922b678382ec307a75102eb54267a
ST10033851 <ST10033851@vcconnect.edu.za>
2024-10-07 21:44:44 +0200

    Updated config.yml file

--- PalPayPaymentsPlatform/backend/sample.test.js
+++
@@ -1,5 +0,0 @@
-//Simple test for the backend
-test('Sample test', () => {
-    expect(1 + 1).toBe(2);
-  });
-  
--- PalPayPaymentsPlatform/frontend/src/app.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-//Simple test to check if the app renders a link element
-test('renders test link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/test/i);
-  expect(linkElement).toBeInTheDocument();
-});
